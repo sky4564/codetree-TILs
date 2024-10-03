94ca64f0187661c1dc96b5677b5d0bd3ca6d15e6
김병민 <dobe4564@naver.com>
2024-10-03 21:14:03 +0900

    Title: a 부터 b 까지, Time: 195ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241003/a 부터 b 까지/a-to-b.js
@@ -0,0 +1,38 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const input = []
+
+rl.on('line', function(line){
+    input.push(line)
+})
+rl.on('close', function(){
+    console.log(sol())
+})
+
+
+// 하나씩 올라가면서 조건에 만족하는건 엔서에 푸쉬
+
+function sol() {
+    let ans = ""
+    const [a,b] = input[0].split(' ').map(Number)    
+    for(let i  = a ; i <= b; i++){        
+        if(i == a) ans += `${i} `        
+        if(i%2 == 0) {
+            if(i+3 <= b){
+                ans += `${i+3} `
+                i += 2
+            }            
+        }
+        else if ( i % 2 !== 0){
+            if(i*2 <= b){
+                ans += `${2*i} `
+                i = 2*i - 1     
+            }
+        }
+    }
+    return ans
+}
